c6ea4828cb77f8b7f8f8176a922a1a480f6057ea
Ishant Kumar <er.ishant@gmail.com>
2019-05-02 02:19:15 +0530

    Session 19 - PreparedStatement API

--- Session19/src/com/auribises/main/App.java
+++ Session19/src/com/auribises/main/App.java
@@ -2,6 +2,8 @@ package com.auribises.main;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.Scanner;
 
@@ -46,7 +48,7 @@ public class App {
 		// 1. Java File IO APIs
 		// 2. JDBC | Java Data Base Connectivity
 		
-		Customer cRef1 = new Customer();
+		/*Customer cRef1 = new Customer();
 		
 		Scanner scanner = new Scanner(System.in);
 		
@@ -59,7 +61,7 @@ public class App {
 		System.out.println(">> Enter Customer Email: ");
 		cRef1.email = scanner.nextLine();
 	
-		System.out.println(cRef1);
+		System.out.println(cRef1);*/
 		
 		
 		try {
@@ -75,19 +77,49 @@ public class App {
 			System.out.println(">> 2. Connection Created");
 			
 			// 3. Write SQL Statement
-			String sql = "insert into Customer values(null, '"+cRef1.name+"', '"+cRef1.phone+"', '"+cRef1.email+"', "+cRef1.age+")";
+			//String sql = "insert into Customer values(null, '"+cRef1.name+"', '"+cRef1.phone+"', '"+cRef1.email+"', "+cRef1.age+")";
+			//String sql = "insert into Customer values(null, ?, ?, ?, ?)";
+			//String sql = "delete from Customer where cid = ?";
+			
+			String sql = "select * from Customer";
+			//String sql = "select * from Customer where name = ?";
+			
 			System.out.println(">> 3. SQL Statement: "+sql);
 			
 			
 			// 4. Execute SQL Statement
-			Statement stmt = con.createStatement();
-			int i = stmt.executeUpdate(sql); // Insert, Update and Delete
+			//Statement stmt = con.createStatement();
+			//int i = stmt.executeUpdate(sql); // Insert, Update and Delete
 			
-			if(i>0){
+			PreparedStatement stmt = con.prepareStatement(sql);
+//			stmt.setString(1, cRef1.name);
+//			stmt.setString(2, cRef1.phone);
+//			stmt.setString(3, cRef1.email);
+//			stmt.setInt(4, cRef1.age);
+			
+			//stmt.setInt(1, 3);
+			
+			//int i = stmt.executeUpdate();
+			
+			/*if(i>0){
 				System.out.println(">> 4. SQL Statement Executed");
 			}else{
 				System.out.println(">> 4. SQL Statement Failed");
+			}*/
+			
+			ResultSet rs = stmt.executeQuery(); // Select Operation or Fetch Data from DataBase
+			
+			while(rs.next()){
+				Customer c = new Customer();
+				c.cid = rs.getInt(1);
+				c.name = rs.getString(2);
+				c.phone = rs.getString(3);
+				c.email = rs.getString(4);
+				c.age = rs.getInt(5);
+
+				System.out.println(c); // c.toString()
 			}
+			
 			
 			// 5. Close the Connection
 			con.close();
